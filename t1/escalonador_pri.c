32cf6cc1c6b71123eb556cb48e1b427e0624b1f6
Hugo Grochau <hugo@grochau.com>
2014-10-09 12:16:16 -0300

    Fix round robin scheduler

--- t1/escalonador_pri.c
+++ t1/escalonador_pri.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include "interpreter.h"
 #define INTERVAL 1 //ms
 
@@ -12,18 +15,60 @@ int compare(const void *a, const void *b) {
     return pa->priority > pb->priority;
 }
 
+void remove_from_array(Process *p_pool, int index, int size) {
+    int i;
+    for (i = index; i < size - 1; i++)
+        p_pool[index] = p_pool[index + 1];
+}   
+
+int pid_exists(int pid) {
+    int status;
+    return pid != waitpid(pid, &status, WNOHANG);
+}
+
 int main(void) {
+
     Process p_pool[MAX_POOL_SIZE];
-    int i, status;
     int num_process = interpret(p_pool, PRI, stdin);
+    int current = -1;
+
+
     qsort(p_pool, num_process, sizeof(Process), compare);
-    for (i = 0; i < num_process; i++) {
-        printf("pid: %d, prioridade: %d\n", p_pool[i].pid, p_pool[i].priority);
-        if (i > 0)
-            kill(p_pool[i-1].pid, SIGKILL);
-        kill(p_pool[i].pid, SIGCONT);
-        waitpid(p_pool[i].pid, &status, 0);
+
+    while (1) {
+
+        printf("num_process: %d \n", num_process);
+        if (num_process == 0) // no processes to execute
+            break;
+
+        if (num_process > 1 && current >= 0 && current != p_pool[0].pid) {
+            printf("Parando o processo de PID: %d \n",  current);
+
+            kill(current, SIGSTOP);
+        }
+
+        current = p_pool[0].pid;
+
+        if (pid_exists(p_pool[0].pid)) {
+
+            printf("Executando e diminuindo a prioridade do processo de PID: %d e prioridade: %d \n",  p_pool[0].pid, p_pool[0].priority);
+
+            kill(p_pool[0].pid, SIGCONT);
+            if (p_pool[0].priority < 7) 
+                p_pool[0].priority++;
+
+        } else {
+            printf("Removendo o processo de PID: %d \n", p_pool[0].pid);
+            remove_from_array(p_pool, 0, num_process);
+            num_process--;
+            current = -1;
+        }
+
+        sleep(INTERVAL);
+
+        if (num_process > 1)
+            qsort(p_pool, num_process, sizeof(Process), compare);
+
     }
-    kill(p_pool[i].pid, SIGKILL);
     return 0;
 }
--- t1/escalonador_rr.c
+++ t1/escalonador_rr.c
@@ -2,6 +2,8 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 #include "interpreter.h"
 #define INTERVAL 1 //ms
@@ -13,10 +15,8 @@ void remove_from_array(Process *p_pool, int index, int size) {
 }   
 
 int pid_exists(int pid) {
-    struct stat sts;
-    char pid_path[16];
-    sprintf(pid_path, "/proc/%d", pid);
-    return stat(pid_path, &sts) == 0;
+    int status;
+    return pid != waitpid(pid, &status, WNOHANG);
 }
 
 int main(void) {
@@ -33,10 +33,18 @@ int main(void) {
         
         prev = cur > 0 ? cur - 1 : num_process - 1;
       
-        kill(p_pool[prev].pid, SIGSTOP);
+        if (num_process > 1) {
+            kill(p_pool[prev].pid, SIGSTOP);
+        }        
 
-        kill(p_pool[cur].pid, SIGCONT);
-
+        if (pid_exists(p_pool[cur].pid)) {
+            kill(p_pool[cur].pid, SIGCONT);
+        } else {
+            remove_from_array(p_pool, cur, num_process);
+            num_process--;
+            cur--;
+        }
+        
 
         sleep(INTERVAL);
         cur++;
