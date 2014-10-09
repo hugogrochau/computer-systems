ef20e758de44eda88d67a464629c7db8166f6b40
LORENZO PALERMO SARAIVA <c1110649@tocantins.grad.inf.puc-rio.br>
2014-10-09 11:53:22 -0300

    Add debug messages for sjf

--- t1/escalonador_sjf.c
+++ t1/escalonador_sjf.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "interpreter.h"
 #define INTERVAL 1 //ms
 
@@ -15,15 +16,29 @@ int compare(const void *a, const void *b) {
 int main(void) {
     Process p_pool[MAX_POOL_SIZE];
     int i, status;
-    int num_process = interpret(p_pool, SJF, stdin);
+	int output = open("output.txt", O_RDWR | O_CREAT, 0666);
+	int input = open("input.txt",  O_RDWR | O_CREAT, 0666);
+
+	close(0);
+	dup(input);
+
+	close(1);
+	dup(output);
+
+    int num_process = interpret(p_pool, SJF);
     qsort(p_pool, num_process, sizeof(Process), compare);
     for (i = 0; i < num_process; i++) {
-        printf("pid: %d, tempoexec: %lf\n", p_pool[i].pid, p_pool[i].time);
-        if (i > 0)
+        if (i > 0){
             kill(p_pool[i-1].pid, SIGKILL);
+			dprintf(output, "Processo de PID: %d e duração: %lf terminou de executar\n",  p_pool[i-1].pid, p_pool[i-1].time);
+	}
+		dprintf(output, "Processo de PID: %d e duração: %lf começou a executar\n",  p_pool[i].pid, p_pool[i].time);
         kill(p_pool[i].pid, SIGCONT);
+		
         waitpid(p_pool[i].pid, &status, 0);
     }
-    kill(p_pool[i].pid, SIGKILL);
+	dprintf(output, "Processo de PID: %d e Prioridade: %lf terminou de executar. \n", p_pool[i-1].pid, p_pool[i-1].time);
+    kill(p_pool[i].pid, SIGKILL);	
+	dprintf(output, "Todos os processos terminaram de executar. \n");
     return 0;
 }
